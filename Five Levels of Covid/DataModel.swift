5bbb77e93639f734dd278d4fda6f6a49a5a555db
paulba71 <paulba71@hotmail.com>
2020-10-20 19:30:16 +0100

    Added some extra values and current situation

--- Five Levels of Covid/DataModel.swift
+++ Five Levels of Covid/DataModel.swift
@@ -21,10 +21,10 @@ let level1SecondItem = ["from 3 households",
                          "restrictions",
                          "off-peak hours or walk/cycle, if possible"]
 
-let level1Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "Sports", "cafes", "pubs", "pubs", "pubs"]
+let level1Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "Sports", "cafes", "pubs", "travel", "publictransport"]
 
 // Level 2
-let level2FirstItem = ["6", "6", "15", "50", "50", "50", "100", "Open", "Open"]
+let level2FirstItem = ["6", "6", "15", "50", "50", "50", "100", "Open", "Open", "No", "Peak hours"]
 
 let level2SecondItem = ["from 2 or 3 households",
                          "indoor",
@@ -34,12 +34,14 @@ let level2SecondItem = ["from 2 or 3 households",
                          "indoor",
                          "outdoor",
                          "6 people from up to 3 households",
-                         "Groups of 6 from up to 3 households"]
+                         "Groups of 6 from up to 3 households",
+                         "restrictions",
+                         "essential workers / purposes only; 50% capacity"]
 
-let level2Image = ["Visitors", "Gatherings", "Gatherings", "Weddings", "Indoor", "Sports", "Sports", "cafes", "pubs"]
+let level2Image = ["Visitors", "Gatherings", "Gatherings", "Weddings", "Indoor", "Sports", "Sports", "cafes", "pubs",  "travel", "publictransport"]
 
 // Level 3
-let level3FirstItem = ["One", "No", "25", "No", "No", "Open", "Open"]
+let level3FirstItem = ["One", "No", "25", "No", "No", "Open", "Open", "Stay", "All Hours"]
 
 let level3SecondItem = ["other household",
                          "social of family gatherings",
@@ -47,12 +49,14 @@ let level3SecondItem = ["other household",
                          "indoor gatherings",
                          "matches or events",
                          "Additional restrictions for indoor dining",
-                         "Additional restrictions"]
+                         "Additional restrictions",
+                         "in your own county / region apart from work/education/essential purposes",
+                         "essential workers / purposes only; 50% capacity"]
 
-let level3Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs"]
+let level3Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs", "travel", "publictransport"]
 
 // Level 4
-let level4FirstItem = ["No", "No", "6", "No", "No", "Open", "Open"]
+let level4FirstItem = ["No", "No", "6", "No", "No", "Open", "Open", "Stay", "Avoid"]
 
 let level4SecondItem = ["visitors permitted",
                          "indoor gatherings",
@@ -60,12 +64,14 @@ let level4SecondItem = ["visitors permitted",
                          "indoor gatherings",
                          "matches or events",
                          "Take away or delivery only. Outdoor dining to 15 people",
-                         "Outdoor only limited to a max of 15"]
+                         "Outdoor only limited to a max of 15",
+                         "in your own county / region apart from work/education/essential purposes",
+                         "public transport; essential workers/purposes only; 25% capactity"]
 
-let level4Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs"]
+let level4Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs", "travel", "publictransport"]
 
 // Level 5
-let level5FirstItem = ["No", "No", "6", "No", "No", "Open", "Open"]
+let level5FirstItem = ["No", "No", "6", "No", "No", "Open", "Open", "Stay", "Avoid"]
 
 let level5SecondItem = ["visitors permitted",
                          "excpet funerals or weddings",
@@ -73,7 +79,9 @@ let level5SecondItem = ["visitors permitted",
                          "indoor gatherings",
                          "matches or events",
                          "Take away food or delivery only",
-                         "offer takeaway or deliver service"]
+                         "offer takeaway or deliver service",
+                         "at home except to exercise within 5K of home",
+                         "public transport; essential workers/purposes only; 25% capactity"]
 
-let level5Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs"]
+let level5Image = ["Visitors", "Gatherings", "Weddings", "Indoor", "Sports", "cafes", "pubs", "travel", "publictransport"]
 
